752147bd776680f00b83f05e25f814e170536064
Ian McCallum <ianfmc@gmail.com>
2016-10-27 16:35:42 -0700

    Add package.json and unit test

---
+++ spec/delete.spec.js
@@ -0,0 +1,14 @@
+var AWSMock = require('aws-sdk-mock');
+var AWS = require('aws-sdk');
+
+var app = require('../index.js');
+var sinon = require('sinon');
+
+var expect = require('chai').expect;
+var should = require('chai').should;
+var assert = require('chai').assert;
+
+describe('Delete a Team', function() { 
+});
+
+
